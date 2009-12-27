b3022d798deba7e3399b9d28ec0b0f25787faf30
tbruyelle <tom@TOMPORTABLE.(none)>
2009-12-27 01:16:22 +0100

    change black list element subtitle

--- src/com/tsoft/happycontacts/blacklist/BlackListActivity.java
+++ src/com/tsoft/happycontacts/blacklist/BlackListActivity.java
@@ -1,15 +1,16 @@
 /**
  * 
  */
-package com.tsoft.happycontacts;
+package com.tsoft.happycontacts.blacklist;
 
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
+import com.tsoft.happycontacts.Log;
+import com.tsoft.happycontacts.R;
 import com.tsoft.happycontacts.dao.DbAdapter;
 import com.tsoft.happycontacts.dao.HappyContactsDb;
 
@@ -71,24 +72,15 @@ public class BlackListActivity
     {
         mCursorBlakListed = mDb.fetchAllBlackList();
         startManagingCursor( mCursorBlakListed );
-
-        String[] from =
-            new String[] { HappyContactsDb.BlackList.CONTACT_NAME, HappyContactsDb.BlackList.LAST_WISH_YEAR };
-        //    int[] to = new int[] { R.id.contact_name, R.id.last_wish_year };
-        //    SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,
-        //        R.layout.blacklist_element, c, from, to);
-        int[] to = new int[] { R.id.contact_name, R.id.last_wish_year };
-        SimpleCursorAdapter simpleCursorAdapter =
-            new SimpleCursorAdapter( this, R.layout.blacklist_element, mCursorBlakListed, from, to );
-        setListAdapter( simpleCursorAdapter );
+        setListAdapter( new BlackListCursorAdapter( this, mCursorBlakListed ) );
     }
 
     @Override
     protected void onListItemClick( ListView l, View v, int position, long id )
     {
         super.onListItemClick( l, v, position, id );
-        Long blackListId =
-            mCursorBlakListed.getLong( mCursorBlakListed.getColumnIndexOrThrow( HappyContactsDb.BlackList.ID ) );
+        Long blackListId = mCursorBlakListed.getLong( mCursorBlakListed
+            .getColumnIndexOrThrow( HappyContactsDb.BlackList.ID ) );
         mDb.deleteBlackList( blackListId );
         fillList();
     }
---
+++ src/com/tsoft/happycontacts/blacklist/BlackListCursorAdapter.java
@@ -0,0 +1,54 @@
+package com.tsoft.happycontacts.blacklist;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.tsoft.happycontacts.R;
+import com.tsoft.happycontacts.dao.HappyContactsDb;
+
+/**
+ * @author tom
+ * Custom cursor adapter to display or not the value of lastwishdate column.
+ */
+public class BlackListCursorAdapter
+    extends CursorAdapter
+{
+    LayoutInflater mInflater;
+
+    public BlackListCursorAdapter( Context context, Cursor c )
+    {
+        super( context, c );
+        mInflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+    }
+
+    @Override
+    public void bindView( View view, Context context, Cursor cursor )
+    {
+        // contact name
+        TextView contactNameTextView = (TextView) view.findViewById( R.id.contact_name );
+        String contactName = cursor.getString( cursor.getColumnIndex( HappyContactsDb.BlackList.CONTACT_NAME ) );
+        contactNameTextView.setText( contactName );
+
+        // last wish date
+        TextView lastWishDateTextView = (TextView) view.findViewById( R.id.last_wish_year );
+        String lastWishDate = cursor.getString( cursor.getColumnIndex( HappyContactsDb.BlackList.LAST_WISH_YEAR ) );
+        if ( lastWishDate != null )
+        {
+            lastWishDateTextView.setText( context.getString( R.string.last_wish_date, lastWishDate ) );
+        }
+    }
+
+    @Override
+    public View newView( Context context, Cursor cursor, ViewGroup parent )
+    {
+        View view = mInflater.inflate( R.layout.blacklist_element, null );
+        bindView( view, context, cursor );
+        return view;
+    }
+
+}
