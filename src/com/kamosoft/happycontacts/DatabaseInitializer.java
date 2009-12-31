9dffc9580bbe11e371819dc44b88cd85e23a28f5
TBRUYELLE <TBRUYELLE@W-MVASQUEZ.eu.accor.net>
2009-12-31 15:48:45 +0100

    try progress bar

--- src/com/kamosoft/happycontacts/DatabaseInitializer.java
+++ src/com/kamosoft/happycontacts/DatabaseInitializer.java
@@ -1,9 +1,8 @@
 package com.kamosoft.happycontacts;
 
 import android.app.ProgressDialog;
-import android.content.Context;
-
-import com.kamosoft.happycontacts.dao.DbAdapter;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 
 /**
  * Threaded database initializer to avoid latence when db create or update
@@ -13,36 +12,70 @@ import com.kamosoft.happycontacts.dao.DbAdapter;
  * @version $Id$
  */
 public class DatabaseInitializer
-    extends Thread
+    extends AsyncTask<String, Integer, Integer>
 {
-    private Context mContext;
+    private SQLiteDatabase mDb;
 
     private ProgressDialog mProgressDialog;
 
-    public DatabaseInitializer( Context context, ProgressDialog progressDialog )
+    public DatabaseInitializer( SQLiteDatabase db, ProgressDialog progressDialog )
     {
         super();
-        mContext = context;
+        mDb = db;
         mProgressDialog = progressDialog;
+    }
+
+    /**
+     * @see android.os.AsyncTask#doInBackground(Params[])
+     */
+    @Override
+    protected Integer doInBackground( String... sqlStatements )
+    {
+        int count = sqlStatements.length;
+        SQLiteDatabase db = mDb;
+        for ( int i = 0; i < count; i++ )
+        {
+            db.execSQL( sqlStatements[i] );
+            publishProgress( (int) ( ( i / (float) count ) * 100 ) );
+        }
+        return 0;
+    }
+
+    /**
+     * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+     */
+    @Override
+    protected void onPostExecute( Integer result )
+    {
+        mProgressDialog.dismiss();
+    }
+
+    /**
+     * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+     */
+    @Override
+    protected void onProgressUpdate( Integer... values )
+    {
+        mProgressDialog.setProgress( values[0].intValue() );
     }
 
     /**
      * @see java.lang.Thread#run()
      */
-    @Override
-    public void run()
-    {
-        try
-        {
-            DbAdapter dbAdapter = new DbAdapter( mContext );
-            /* a simple call to open() proceed to db create or upgrade */
-            dbAdapter.open( true );
-            dbAdapter.close();
-        }
-        finally
-        {
-            mProgressDialog.dismiss();
-        }
-    }
+    //    @Override
+    //    public void run()
+    //    {
+    //        try
+    //        {
+    //            DbAdapter dbAdapter = new DbAdapter( mContext );
+    //            /* a simple call to open() proceed to db create or upgrade */
+    //            dbAdapter.open( true );
+    //            dbAdapter.close();
+    //        }
+    //        finally
+    //        {
+    //            mProgressDialog.dismiss();
+    //        }
+    //    }
 
 }
