fe57cc3923b2d2c9adb881ac886ff1be085189a5
TBRUYELLE <TBRUYELLE@W-MVASQUEZ.eu.accor.net>
2010-01-05 10:13:45 +0100

    update db with other source

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -6,12 +6,13 @@ wget http://www.ephemeride.name/index.php?jourR=$1-$2 -O $file -o $log
 perl -ne '/galement les\.\.\.<.*?>\.(.*?)\./ && print "$1\n"' $file > $file.tmp
 perl -i -pe 's/\s//g'  $file.tmp
 perl -i -pe 's/-/\n/g' $file.tmp
-mv $file.tmp $file.ok
+iconv -f utf-8 -t iso-8859-1 $file.tmp > $file.ok
 nb=`wc -l $file.ok`
 echo "extracted $nb names..."
 for name in `cat $file.ok`
 do
-        echo "insert into feast (name, day) values (\"$name\", \"$1/$2\");" >> $ath/names.sql
+        echo "insert into feast (name, day, source) values (\"$name\", \"$1/$2\", \"ephemeride.name\");" >> $path/names.sql
 done
 rm $path/*.bak
+rm $path/*.tmp
 exit 0
