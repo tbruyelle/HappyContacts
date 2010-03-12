35d70ae400276fbb5a855bd8d3c15a6ac0122c41
tbruyelle <tom@TOMPORTABLE.(none)>
2010-03-13 00:17:35 +0100

    prepare v1.2

--- src/com/kamosoft/happycontacts/Log.java
+++ src/com/kamosoft/happycontacts/Log.java
@@ -5,22 +5,22 @@ package com.kamosoft.happycontacts;
 
 public class Log
 {
-  public final static String LOGTAG = "HappyContacts";
+    public final static String LOGTAG = "HappyContacts";
 
-  public static final boolean DEBUG = false;
+    public static final boolean DEBUG = false;
 
-  public static void v(String msg)
-  {
-    android.util.Log.v(LOGTAG, msg);
-  }
+    public static void v( String msg )
+    {
+        android.util.Log.v( LOGTAG, msg );
+    }
 
-  public static void e(String msg)
-  {
-    android.util.Log.e(LOGTAG, msg);
-  }
+    public static void e( String msg )
+    {
+        android.util.Log.e( LOGTAG, msg );
+    }
 
-  public static void e(String msg, Throwable tr)
-  {
-    android.util.Log.e(LOGTAG, msg, tr);
-  }
+    public static void e( String msg, Throwable tr )
+    {
+        android.util.Log.e( LOGTAG, msg, tr );
+    }
 }
