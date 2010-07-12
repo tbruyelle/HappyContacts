8f99c7dd3a43c82b087c80893861c71dc50e3a72
TBRUYELLE <TBRUYELLE@W-MVASQUEZ.eu.accor.net>
2010-07-12 12:01:43 +0200

    move common methods to abstract class

---
+++ src/com/kamosoft/happycontacts/contacts/AbstractContactProxy.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) Kamosoft 2010
+ */
+package com.kamosoft.happycontacts.contacts;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.Cursor;
+
+/**
+ * @author <a href="mailto:thomas.bruyelle@accor.com">tbruyelle</a>
+ *
+ * @since 12 juil. 2010
+ * @version $Id$
+ */
+public abstract class AbstractContactProxy
+    implements IContactProxy
+{
+    /**
+     * @see com.kamosoft.happycontacts.contacts.IContactProxy#doQuery(android.content.Context, java.lang.String, java.lang.String)
+     */
+    public Cursor doQuery( Context context, String where )
+    {
+        if ( context == null )
+        {
+            return null;
+        }
+        return context.getContentResolver().query( getContentUri(), new String[] { getIdColumn(), getNameColumn() },
+                                                   where, null, getNameColumn() + " ASC" );
+    }
+
+    /**
+     * @see com.kamosoft.happycontacts.contacts.IContactProxy#loadPhoneContacts(android.content.Context)
+     */
+    public ArrayList<PhoneContact> loadPhoneContacts( Context context )
+    {
+        ArrayList<PhoneContact> phoneContacts = new ArrayList<PhoneContact>();
+        Cursor cursor = doQuery( context, null );
+        if ( cursor == null )
+        {
+            return phoneContacts;
+        }
+
+        while ( cursor.moveToNext() )
+        {
+            PhoneContact contact = createFromCursor( cursor );
+            if ( contact == null )
+            {
+                continue;
+            }
+            phoneContacts.add( contact );
+        }
+
+        cursor.close();
+        return phoneContacts;
+    }
+
+    /**
+     * @param cursor
+     * @return
+     */
+    protected PhoneContact createFromCursor( Cursor cursor )
+    {
+        if ( cursor == null || cursor.isClosed() )
+        {
+            return null;
+        }
+
+        Long id = cursor.getLong( cursor.getColumnIndex( getIdColumn() ) );
+        String name = cursor.getString( cursor.getColumnIndex( getNameColumn() ) );
+        if ( name == null || name.length() == 0 )
+        {
+            return null;
+        }
+        return new PhoneContact( id, name );
+    }
+}
--- src/com/kamosoft/happycontacts/contacts/ContactProxy.java
+++ src/com/kamosoft/happycontacts/contacts/ContactProxy.java
@@ -17,73 +17,15 @@ import com.kamosoft.happycontacts.R;
 
 @SuppressWarnings( "deprecation" )
 public class ContactProxy
+    extends AbstractContactProxy
     implements IContactProxy
 {
-    /**
-     * @see com.kamosoft.happycontacts.contacts.IContactProxy#doQuery(android.content.Context, java.lang.String, java.lang.String)
-     */
-    public Cursor doQuery( Context context, String where )
-    {
-        if ( context == null )
-        {
-            return null;
-        }
-        return context.getContentResolver().query( getContentUri(), new String[] { getIdColumn(), getNameColumn() },
-                                                   where, null, getNameColumn() + " ASC" );
-    }
-
     /**
      * @see com.kamosoft.happycontacts.contacts.IContactProxy#getContentUri()
      */
     public Uri getContentUri()
     {
         return People.CONTENT_URI;
-    }
-
-    /**
-     * @param cursor
-     * @return
-     */
-    protected PhoneContact createFromCursor( Cursor cursor )
-    {
-        if ( cursor == null || cursor.isClosed() )
-        {
-            return null;
-        }
-
-        Long id = cursor.getLong( cursor.getColumnIndex( getIdColumn() ) );
-        String name = cursor.getString( cursor.getColumnIndex( getNameColumn() ) );
-        if ( name == null || name.length() == 0 )
-        {
-            return null;
-        }
-        return new PhoneContact( id, name );
-    }
-
-    /**
-     * @see com.kamosoft.happycontacts.contacts.IContactProxy#loadPhoneContacts(android.content.Context)
-     */
-    public ArrayList<PhoneContact> loadPhoneContacts( Context context )
-    {
-        ArrayList<PhoneContact> phoneContacts = new ArrayList<PhoneContact>();
-        Cursor cursor = doQuery( context, null );
-        if ( cursor == null )
-        {
-            return phoneContacts;
-        }
-
-        while ( cursor.moveToNext() )
-        {
-            PhoneContact contact = createFromCursor( cursor );
-            if ( contact == null )
-            {
-                continue;
-            }
-            phoneContacts.add( contact );
-        }
-
-        cursor.close();
-        return phoneContacts;
     }
 
     /**
