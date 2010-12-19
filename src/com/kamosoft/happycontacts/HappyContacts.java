dcf907b1c1bd56665e3bae0ff27d29c891ec2b5d
tbruyelle <thomas.bruyelle@gmail.com>
2010-12-19 21:57:03 +0100

    add dashboard

--- src/com/kamosoft/happycontacts/HappyContacts.java
+++ src/com/kamosoft/happycontacts/HappyContacts.java
@@ -4,6 +4,9 @@
 package com.kamosoft.happycontacts;
 
 import greendroid.app.GDActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author tom
@@ -13,4 +16,22 @@ public class HappyContacts
     extends GDActivity
 {
 
+    /**
+     * @see greendroid.app.GDActivity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setActionBarContentView( R.layout.dashboard );
+    }
+
+    /**
+     * Handle Alarm button click
+     * @param v
+     */
+    public void onAlarmClick( View v )
+    {
+        Toast.makeText( this, "yeah", Toast.LENGTH_SHORT ).show();
+    }
 }
