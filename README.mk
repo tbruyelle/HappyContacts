5b327c54d66b69210fe0a0058531b148d65177ec
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2011-10-23 00:22:20 +0200

    add readme

---
+++ README.mk
@@ -0,0 +1,4 @@
+=== Dependecies ===
+* libs directory
+* facebook for android : you need to clone the repo https://github.com/facebook/facebook-android-sdk/tree/master/facebook
+and add as a library dependency to HappyContacts
